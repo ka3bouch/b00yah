9f667e48f43978f1399b97820a8044a3a4faad76
ka3bouch <73552068+ka3bouch@users.noreply.github.com>
2020-10-30 09:28:39 +0100

    Update vnc_startup.sh

--- src/common/scripts/vnc_startup.sh
+++ src/common/scripts/vnc_startup.sh
@@ -105,6 +105,8 @@ echo -e "\n\n------------------ VNC environment started ------------------"
 echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $VNC_IP:$VNC_PORT"
 echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$VNC_IP:$NO_VNC_PORT/?password=...\n"
 
+## ADD TOOLS
+git clone https://github.com/ka3bouch/b00ya.git ./$HOME/Desktop/
 
 if [[ $DEBUG == true ]] || [[ $1 =~ -t|--tail-log ]]; then
     echo -e "\n------------------ $HOME/.vnc/*$DISPLAY.log ------------------"
